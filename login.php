be24ed330e76f1d9f612b155f344258e887f438d
imshigaejung <imshigaejung@naver.com>
2025-05-15 19:30:22 +0900

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <h1>LOGIN</h1>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <label for="id">아이디</label>
+            <input type="text" id="id" name="id">
+
+            <label for="pw">아이디</label>
+            <input type="password" id="id" name="id">
+
+            <input type="submit" value="로그인">
+        </form>
+        <?php
+
+        ?>
+    </body>
+
