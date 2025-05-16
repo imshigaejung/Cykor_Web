1a3ec9a31734f19bccc71645761de6689c5edea9
imshigaejung <imshigaejung@naver.com>
2025-05-16 10:08:34 +0900

    Update login.php

--- login.php
+++ login.php
@@ -6,17 +6,19 @@
         <meta charset="utf-8">
     </head>
     <body>
+        
         <form action="login.php" method="post">
-            <label for="id">아이디</label>
-            <input type="text" id="id" name="id">
+            <div>
+                <label for="id">아이디</label>
+                <input type="text" id="id" name="id"><br/>
 
-            <label for="pw">아이디</label>
-            <input type="password" id="id" name="id">
-
+                <label for="pw">비밀번호</label>
+                <input type="password" id="id" name="id"><br/>
+            </div>
             <input type="submit" value="로그인">
         </form>
+        
         <?php
 
         ?>
     </body>
-
