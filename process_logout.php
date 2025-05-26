60bd5a25dd8cafef0612612908dc7dd807a539e9
imshigaejung <imshigaejung@naver.com>
2025-05-26 14:14:37 +0900

    Delete process_logout.php

--- process_logout.php
+++
@@ -1,9 +0,0 @@
- <?php
-    echo "test1";
-    session_start();
-    unset($_SESSION['user_id']);
-    unset($_SESSION['user_name']);
-    session_destroy();
-    header("Location: board_login.php");
-    exit;
-?>                               
